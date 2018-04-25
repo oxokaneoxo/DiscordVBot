fc833a6212e10d3a946f68284a730787cf7df9bd
Micheal Earl <michealearldev@gmail.com>
2018-04-24 21:33:55 -0700

    Added discord.js functions, simple bot implemented

--- server.js
+++ server.js
@@ -1,1 +1,12 @@
-console.log("test");
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('message', message => {
+  client.user.setActivity("Matching");
+
+  if(message.content === 'ping') {
+    message.reply("pong")
+  }
+})
+
+client.login("NDM3ODEzMzM4Mjk1MjM4NjU2.Db7kpg.lwHfaM-2mK_yyJVPGUvUBeqfI_s");
